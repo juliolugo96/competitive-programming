b84dc3b05b0791e0435a54ea3713616d325f2fea
Julio Lugo <jmanuellugo96@gmail.com>
2022-08-13 19:49:19 -0400

    Progress with letters.cpp

--- codeforces/two_pointers/letters.cpp
+++ codeforces/two_pointers/letters.cpp
@@ -7,16 +7,40 @@ int main()
   int m, n;
 
   cin >> n >> m;
-  int dorms[n];
-  for (int i{0}; i < n; ++i)
-    cin >> dorms[i];
+  int dorms[n]{0};
   
-  for (int i{0}, j{0}; i < m;++i)
+  for (int i{0}; i < n; ++i) {
+    cin >> dorms[i];
+    if (i > 0) dorms[i] += dorms[i - 1];
+  }
+    
+  for (int i{0}; i < m;++i)
   {
     int rec;
     cin >> rec;
 
+    if (rec == 1)
+    {
+      cout << "1 1\n";
+      continue;
+    }
+
+    int low{0}, high{n-1};
+
+    while (low <= high) {
+      int mid = (low + high) /2;
     
+      if (rec > dorms[mid-1] && rec <= dorms[mid])
+      {
+        cout << mid + 1 << " " << rec - dorms[mid - 1] << "\n";
+        break;
+      }
+
+      if (rec < dorms[mid-1])
+        high = mid - 1;
+      else
+        low = mid + 1;
+    }
 
   }
   
