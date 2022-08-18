96dfabeb437aaf59cdf335eeded274d2ba9638c7
Julio Lugo <jmanuellugo96@gmail.com>
2022-08-18 00:56:53 -0400

    Solve Silly mistake

--- codeforces/implementation/silly_mistake.cpp
+++ codeforces/implementation/silly_mistake.cpp
@@ -2,34 +2,34 @@
 
 using namespace std;
 
-static int a[static_cast<int>(1e6) + 1]={0};
 static bool mem[static_cast<int>(1e6) + 1]={false};
-static bool day_mem[static_cast<int>(1e6) + 1]={false};
-
 
 # define DEBUG  cout << "---DEBUG ---\n";
 # define END_DEBUG cout << "---END DEBUG ---\n\n\n";
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
 
   int n;
   cin >> n;
+  int a[n]{0};
   bool flag = false;
-  int count_events{0}, count{0}, days{0}, event_days[n]={0};
+  int count_events{0}, count{0}, days{0}, event_days[n]={0}, prev_day{-1};
 
+  map<pair<int, int>, bool> daily_map;
   for (int i{0}; i< n; ++i)
   {
     cin >> a[i];
 
-    DEBUG
-    cout << "a[" << i << "]: " << a[i] << "\n";
-    cout << "Events count: " << count_events << "\n";
-    cout << "Employeer in office: " << count << "\n";
-    cout << "Current days: " << days << "\n";
-    END_DEBUG
-
-
+    // DEBUG
+    // cout << "a[" << i << "]: " << a[i] << "\n";
+    // cout << "Events count: " << count_events << "\n";
+    // cout << "Employeer in office: " << count << "\n";
+    // cout << "Current days: " << days << "\n";
+    // END_DEBUG
 
     if (flag) continue;
 
@@ -48,13 +48,13 @@ int main()
 
     mem[abs(a[i])] = a[i] > 0;
 
-    if (day_mem[abs(a[i])] && a[i] > 0)
+    if (a[i] > 0 and daily_map[{a[i], days}])
     {
       flag = true;
       continue;
     }
 
-    if (a[i] > 0) day_mem[abs(a[i])] = true;
+    if (a[i] > 0) daily_map[{abs(a[i]), days}] = true;
 
     count_events++;
     mem[abs(a[i])] ? count++ : count--;
@@ -62,24 +62,14 @@ int main()
     if (not count)
     {
       event_days[days] = count_events;
-      int j{0};
-
-      cout << "Count events: " << count_events << "\n";
-      while(count_events)
-      {
-        if (day_mem[j])
-        {
-          day_mem[j] = false;
-          count_events--;
-        }
-        j++;
-      }
       
+      count_events = 0;
       days++;
+      prev_day++;
     }
   }
 
-  if (flag)
+  if (flag or count > 0)
   {
     cout << "-1\n";
     return 0;
