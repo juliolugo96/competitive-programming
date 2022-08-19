52bdf928b941095a387f5abed1fe8d8d5e0912a7
Julio Lugo <jmanuellugo96@gmail.com>
2022-08-19 18:02:23 -0400

    Start farewell party

---
+++ codeforces/constructive/farewell_party.cpp
@@ -0,0 +1,24 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+# define io_boost ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+static int a_map[static_cast<int>(1e5)+1];
+
+int main()
+{
+  io_boost
+  int n;
+  cin >> n;
+  int a[n]{0};
+  bool flag;
+  for (int i{0}; i < n; ++i)
+  {
+    cin >> a[i];
+
+  }
+
+
+  return 0;
+}
