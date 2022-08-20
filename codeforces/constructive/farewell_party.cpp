7d92ba171cde94073ef26977f2ef3852bf36f2df
Julio Lugo <jmanuellugo96@gmail.com>
2022-08-20 00:36:20 -0400

    First second WA farewell party

--- codeforces/constructive/farewell_party.cpp
+++ codeforces/constructive/farewell_party.cpp
@@ -1,24 +1,46 @@
 # include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 # define io_boost ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
-
-static int a_map[static_cast<int>(1e5)+1];
-
+ 
+map<pair<int, int>, int> map_a;
+ 
 int main()
 {
   io_boost
   int n;
   cin >> n;
   int a[n]{0};
-  bool flag;
+ 
+  int start{1}, acc{0};
+ 
   for (int i{0}; i < n; ++i)
   {
     cin >> a[i];
-
+    
+    map_a[{start, a[i]}]++;
+ 
+    if (map_a[{start, a[i]}] == n - a[i])
+      start++;
   }
-
-
+ 
+  string s;
+ 
+  for (auto &p: map_a)
+  {
+    if (n - p.first.second != p.second)
+    {
+      cout << "Impossible\n";
+      return 0;
+    }
+ 
+    for (int i{0}; i < p.second; i++)
+      s += to_string(p.first.first) + ' ';
+  }
+ 
+  cout << "Possible\n";
+  cout << s << "\n";
+ 
   return 0;
 }
