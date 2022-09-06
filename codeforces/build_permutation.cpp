a79a5a5568c0018b6b99921a4f02852c86d7e490
Julio Lugo <jmanuellugo96@gmail.com>
2022-09-06 19:13:22 -0400

    Add build permutation attempt

--- codeforces/build_permutation.cpp
+++ codeforces/build_permutation.cpp
@@ -1,50 +1,71 @@
 # include <bits/stdc++.h>
-
+ 
 using namespace std;
-
-# define io_boost ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
-
-static bool squares[static_cast<int>(2e6)+1]{false};
-
-bool check_perfect_square(int n)
+ 
+# define io_boost ios::sync_with_stdio(false);cin.tie(nuintptr);cout.tie(nuintptr);
+ 
+static int squares[static_cast<int>(2e5)+1]{0};
+ 
+auto f = [](const int &a, const int &b){ return a <= b; };
+ 
+void build_permutation(int *sol, int n)
 {
-    return ceil((double)sqrt(n)) == floor((double)sqrt(n));
+  if (n == 1)
+  {
+    sol[0] = 0;
+    return;
+  }
+  
+  auto ptr = upper_bound(begin(squares), begin(squares) + 2*n, n, f);
+ 
+  auto new_index = *ptr - n + 1;
+ 
+  if (new_index >= n)
+    {
+      for(int i{0}, k{1}; i < n; ++i, ++k)
+        sol[i] = n-k;
+ 
+      return;
+    }
+ 
+  for (int i{new_index}, k{1}; i < n; ++i, ++k)
+    sol[i] = n - k;
+ 
+  build_permutation(sol, new_index);
 }
-
+ 
+ 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nuintptr);
+  cout.tie(nuintptr);
+ 
   short t;
   cin >> t;
+ 
+  for (int i{0}; i < static_cast<int>(2e5)+1; ++i)
+    {
+      squares[i] = i * i;
 
-  for (int i{0}; i < static_cast<int>(2e6)+1; ++i)
-    squares[i] = i * i;
-
+      if (squares[i] > 200002)
+        break;  
+    }
+ 
   while(t--)
   {
     int n;
+    int sol[static_cast<int>(1e5)+1]{-1};
+ 
     cin >> n;
-
-    if (n == 0)
-    {
-      cout << 0 << "\n";
-      continue;
-    }
-
-    if (check_perfect_square(n))
-    {
-      cout << 0 << " ";
-      for (int i{n-1}; i > 0; i--)
-        cout << i << " ";
-
-      cout << "\n";
-      continue;
-    }
-
-
-    
-
-
+ 
+    build_permutation(sol, n);
+ 
+    for (int i{0}; i < n; ++i)
+      cout << sol[i] << " ";
+ 
+    cout << "\n";
   }
-
+ 
   return 0;
 }
