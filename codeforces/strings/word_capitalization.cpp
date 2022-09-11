11ba2af9107d7b5ab620242635e2b6ddfc033d64
Julio Lugo <jmanuellugo96@gmail.com>
2022-09-11 17:53:09 -0400

    Solve word_capitalization

---
+++ codeforces/strings/word_capitalization.cpp
@@ -0,0 +1,16 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  char val[1001];
+
+  scanf("%s", val);
+
+  val[0] = toupper(val[0]);
+
+  cout << val << "\n";
+
+  return 0;
+}
