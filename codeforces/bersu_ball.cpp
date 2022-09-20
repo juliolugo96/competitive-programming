44df527f25bf4675a55ec2f4dd9d7a44e3b83ce6
Julio Lugo <jmanuellugo96@gmail.com>
2022-09-19 23:58:11 -0400

    First attempt BerSU ball

--- codeforces/bersu_ball.cpp
+++ codeforces/bersu_ball.cpp
@@ -2,13 +2,56 @@
 
 using namespace std;
 
-static int a[100]{999}, b[100]{999};
-static int edmons[100][100];
+static int a[100]{-1}, b[100]{-1};
+static int edmons[100][100]{0};
+int n, m;
+
+bool bpm(int boy, bool *seen, int *available_girls)
+{
+  for (int i{0}; i < m; ++i)
+  {
+    if (edmons[boy][i] and !seen[i])
+    {
+      seen[i] = true;
+
+      if (available_girls[i] < 0 or bpm(available_girls[i], seen, available_girls))
+      {
+        available_girls[i] = boy;
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+
+int max_bpm()
+{
+  // cout << "Graph print\n";
+
+  // for (int i{0}; i < n; i++){
+  //   for (int j{0}; j < m; j++)
+  //     cout << edmons[i][j] << " ";
+
+  //   cout << "\n"; 
+  // }
+  
+  int available_girls[m]{-1};
+
+  int counter{0};
+
+  for (int i{0}; i < n; ++i)
+  {
+    bool seen[m]{false};
+
+    if (bpm(i, seen, available_girls)) counter++;
+  }
+
+  return counter;
+}
 
 int main()
 {
-  short n, m;
-
   int a[100];
   cin >> n;
 
@@ -25,8 +68,7 @@ int main()
     for (int j{0}; j < m; j++)
       edmons[i][j] = abs(a[i] - b[j]) <= 1 ? 1 : 0;
 
-  
-  cout << count << "\n";
+  cout << max_bpm() << "\n";
 
   return 0;
 }
