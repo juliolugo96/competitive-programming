2a360a2741890acdb308201dfc196860629c46a4
Julio Lugo <jmanuellugo96@gmail.com>
2022-10-04 23:45:29 -0400

    Continue card construction

--- codeforces/card_constructions.cpp
+++ codeforces/card_constructions.cpp
@@ -7,19 +7,17 @@ int main()
   int t;
 
   cin >> t;
-  long a[1000000001]{0};
+  long a[36518]{0};
   long count{0};
   
   a[1] = 2;
-  for (long i{2}; i < 1000000001; )
-    {
-      a[i] = a[i - 1] + 3*(i - 1) + 2;
+  for (long i{2}; i < 36518; i++)
+    a[i] = (a[i - 1] + 3*(i - 1) + 2);
 
-      if (a[i] > 1000000001){
-        count = i;
-        break;
-      }
-    }
+  for (int i{0}; i < 10; ++i)
+    cout << a[i] << " ";
+
+  cout << "\n";
 
   while(t--)
   {
@@ -32,14 +30,17 @@ int main()
         continue;
       }
     long counter{0};
-    while(n >= 2)
-    {
-      auto pos = lower_bound(begin(a), begin(a) + count, n);
-      counter += pos - begin(a);
-      n -= *pos;
-    }
     
-    cout << counter << "\n";
+    long* pos = lower_bound(begin(a), begin(a) + 36518, n);
+    counter += pos - begin(a) - 1;
+    n -= *pos;
+    
+    cout << "*Pos: " << *pos << "\n";
+    cout << "Pos: " << pos - begin(a) << "\n";
+    cout << "N before: " << n + *pos << "\n";
+    cout << "N after: " << n << "\n";
+
+
   }
   
   return 0;
