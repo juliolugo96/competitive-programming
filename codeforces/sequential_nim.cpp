ebc24ffc50c47bf7c3e86d2af3605bc84f66891e
Julio Lugo <jmanuellugo96@gmail.com>
2022-10-07 23:42:56 -0400

    Solve sequential nim

--- codeforces/sequential_nim.cpp
+++ codeforces/sequential_nim.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+static int a[100001]{0};
+
 int main()
 {
   ios_base::sync_with_stdio(false);
@@ -14,27 +16,26 @@ int main()
   while(t--)
   {
     int n;
-    int a[100001]{0};
 
     cin >> n;
 
-    cin >> a[0];
-
-
-    if (n == 1)
-    {
-      cout << "First\n";
-      continue;
-    }
-    bool first_wins{a[0] > 1};
-    for (int i{1}; i < (n-1); ++i)
+    bool wins{false}, flag{false};
+    
+    for (int i{0}; i < n; ++i)
     {
       cin >> a[i];
-      
-      first_wins = (a[i] > 1 and first_wins) or (a[i] == 1 and not first_wins);
+
+      if ( flag)
+        continue;
+
+      wins = not wins;
+
+      if(a[i] != 1) 
+        flag = true;
+       
     }
-      
-    cout << (first_wins ? "First\n" : "Second\n");
+
+    cout << (wins ? "First\n" : "Second\n");
   }
 
   return 0;
