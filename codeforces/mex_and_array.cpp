180d9ac82d73a6477ac42ceae409eaca32b72f07
Julio Lugo <jmanuellugo96@gmail.com>
2022-10-22 23:52:27 -0400

    Solve mex and array

--- codeforces/mex_and_array.cpp
+++ codeforces/mex_and_array.cpp
@@ -1,24 +1,6 @@
 # include <bits/stdc++.h>
 
 using namespace std;
-#define MOD 1000000007
-
-
-int fast_pow(int base, int power)
-{
-  int res{1};
-
-  while(power > 0)
-  {
-    if (power & 1) res = (res * base)%MOD;
-
-    base = (base * base) % MOD;
-    power >>= 1;
-  }
-
-  return res;
-}
-
 
 int main()
 {
@@ -31,25 +13,17 @@ int main()
     short n;
     cin >> n;
 
-    int a[n];
+    int a[n]{0};
 
     for (int i{0}; i < n; ++i)
       cin >> a[i];
 
-    uint pow_set_size = fast_pow(2, n);
     int sum{0};
 
-    for (int i={0}; i < n; i++)
-    {
-      int subset[n], k{0};
-      for (int j{0}, k{0}; j < (n - i) ; j++, k++)
-      {
-        subset[k] = 
-      }
-    }
+    for (int i{0}; i < n; ++i)
+      sum += (not a[i] ? 2 : 1) * (i + 1) * (n - i);
 
-
-
+    cout << sum << "\n";
   }
 
   return 0;
