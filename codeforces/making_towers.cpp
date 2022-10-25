ac8fdb2bbc74e15a0e77f76e548d37e1761bc6d1
Julio Lugo <jmanuellugo96@gmail.com>
2022-10-24 23:55:49 -0400

    Start making towers

---
+++ codeforces/making_towers.cpp
@@ -0,0 +1,30 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+
+  while(t--)
+  {
+    int n;
+
+    cin >> n; 
+    int c[n]{0};
+
+    for (int i{0}; i < n; i++)
+      cin >> c[i];
+
+    
+
+  }
+
+
+  return 0;
+}
