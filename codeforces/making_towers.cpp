1d6c0f149257a0e7c0f078e9b9208d587b3f8bea
Julio Lugo <jmanuellugo96@gmail.com>
2022-11-01 23:59:35 -0400

    Start making towers

--- codeforces/making_towers.cpp
+++ codeforces/making_towers.cpp
@@ -16,13 +16,31 @@ int main()
     int n;
 
     cin >> n; 
-    int c[n]{0};
+    int c[n]{0}, colors[n]{-1}, ans[n]{0};
 
-    for (int i{0}; i < n; i++)
+    for (int i{0}; i < n; ++i)
       cin >> c[i];
 
-    
+    for (int i{0}; i < n; ++i)
+    {
+      if (colors[ c[i] -1 ] == -1)
+      {
+        colors[ c[i] -1 ] = i % 2;
+        ans[c[i] -1 ]++;
+      }
 
+      if (colors[ c[i] - 1] != (i % 2))
+      {
+        ans[c[i] -1 ]++;
+        colors[ c[i] -1 ] = i % 2;
+      }
+
+    }
+
+    for (int i{0}; i < n; i++)
+      cout << ans[i] << " ";
+
+    cout << "\n";
   }
 
 
