8fc5a5664b680b3b7c348b37a31c1aa226dceaca
Julio Lugo <jmanuellugo96@gmail.com>
2022-11-05 02:03:31 -0400

    Solve save the magazines

--- codeforces/save_the_magazines.cpp
+++ codeforces/save_the_magazines.cpp
@@ -14,29 +14,48 @@ int main()
   {
     int n;
     cin >> n;
-    int a[n]{0}, ans{0};
+    int a[n]{0};
 
     string s;
     cin >> s;
-
+    int max_ans{0}, start{-1};
+    bool find_zero{false};
     for (int i{0}; i < n; i++)
+    {
       cin >> a[i];
 
-    for (int i{1}; i < n; i++)
-      if (s[i] == '1' and s[i - 1] == '0' and a[i - 1] >= a[i])
+      if (s[i] == '0' and not find_zero)
       {
-        s[i] = '0';
-        s[i - 1] = '1';
+        find_zero = true;
+        start = i;
       }
+        
+      if (s[i] == '1' and not find_zero)
+        max_ans += a[i];
+    }
 
-    for (int i{0}; i < n; i++)
-      if (s[i] == '1') ans += a[i];
+    if (start == -1)
+    {
+      cout << max_ans << "\n";
+      continue;
+    }
+      
+    for (int i{start}; i < n; ++i)
+    {
+      int min_num = a[i];
+      int sum = a[i];
 
-    cout << ans << "\n";
+      while(i + 1 < n and s[i + 1] == '1')
+      {
+        min_num = min(a[i + 1], min_num);
+        sum += a[i + 1];
+        i++;
+      }
+      max_ans += sum - min_num;
+    }
 
+    cout << max_ans << "\n";
   }
-  
-
 
   return 0;
 }
