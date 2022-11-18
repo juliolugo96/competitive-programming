02578c11b3fb2d6e17ed8f68e4b8c9817c33a3ef
Julio Lugo <jmanuellugo96@gmail.com>
2022-11-17 23:59:42 -0400

    Solve arrival

--- codeforces/arrival_of_the_general.cpp
+++ codeforces/arrival_of_the_general.cpp
@@ -9,14 +9,25 @@ int main()
   cin >> n;
 
 
-  int a[n]{0};
+  int a[n]{0}, max{-1}, max_pos{-1}, min_pos{-1}, min{101};
   for (int i{0}; i < n; i++)
   {
     cin >> a[i];
+
+    if (a[i] > max)
+    {
+      max = a[i];
+      max_pos = i;
+    }
+
+    if (a[i] <= min)
+    {
+      min = a[i];
+      min_pos = i;
+    }
   }
 
-
-  
+  cout << max_pos + (n - 1 - min_pos - (min_pos < max_pos ? 1 : 0)) << " \n";
 
   return 0;
 }
