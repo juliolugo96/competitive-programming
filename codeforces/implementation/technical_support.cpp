24c1f6062abf2fca0f99b120916895b05de14098
Julio Lugo <jmanuellugo96@gmail.com>
2022-11-18 23:58:24 -0400

    Start technical support

---
+++ codeforces/implementation/technical_support.cpp
@@ -0,0 +1,24 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  short t;
+
+  cin >> t;
+
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    
+    string s;
+    cin >> s;
+
+
+    cout << (s.back() == 'Q' ? "No\n" : "Yes\n");
+  }
+
+  return 0;
+}
