3171ab0511a6d65e77e9362de0c8b37f62429eb0
Julio Lugo <jmanuellugo96@gmail.com>
2022-12-05 00:14:51 -0400

    Solve pangram

--- codeforces/pangram.cpp
+++ codeforces/pangram.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+
+string chk_Pangram(string s)
+{
+    int Arr[26] = { 0 };
+    
+    for (int i = 0; i < s.length(); i++) 
+    {
+        if (isupper(s[i])) 
+           Arr[ s[i] - 'A' ]++;
+        else if (islower(s[i])) 
+            Arr[ s[i] - 'a' ]++;
+    }
+ 
+    // Iterating through array to check for the presence of 0 value
+    for (int i = 0; i < 26; i++) 
+        if(Arr[i] == 0)
+          return "NO";
+    
+ return "YES";
+}
+
 int main()
 {
   int n;
@@ -11,6 +32,7 @@ int main()
   string s;
   cin >> s;
 
+  cout << chk_Pangram(s) << "\n";
 
   return 0;
 }
