f4a2bb4aefe296f1c59f0ccc88fa558c07c31887
Julio Lugo <jmanuellugo96@gmail.com>
2022-12-20 00:14:24 -0400

    Solve hit the lottery

---
+++ codeforces/hit_the_lottery.cpp
@@ -0,0 +1,45 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int n;
+
+  cin >> n;
+
+  int count = 0, curr = 100;
+
+  while(n > 0)
+  {
+    if (n >= 100)
+    {
+      n -= 100;
+      count++;
+    }    
+    else if (n >= 20)
+    {
+      n -= 20;
+      count++;
+    }
+    else if (n >= 10)
+    {
+      n -= 10;
+      count++;
+    }
+    else if (n >= 5)
+    {
+      n -= 5;
+      count++;
+    }
+    else if (n >= 1)
+    {
+      n -= 1;
+      count++;
+    }
+  }
+
+
+  cout << count << "\n";
+  return 0;
+}
