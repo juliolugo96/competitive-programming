6dcc1b8d8cc50c3dbee30e3d30525788ac165ac4
Julio Lugo <jmanuellugo96@gmail.com>
2023-01-24 23:59:37 -0400

    Solve candies and two sisters

---
+++ codeforces/candies_and_two_sisters.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int t;
+
+  cin >> t;
+
+  while (t--)
+  {
+    int n;
+
+    cin >> n;
+
+    cout << (n & 1 ? n / 2 : n / 2 - 1) << "\n";
+  }
+
+  return 0;
+}
