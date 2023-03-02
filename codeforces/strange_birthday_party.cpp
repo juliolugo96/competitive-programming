2ef37948f9386ad33e24bacd323683693c821e18
Julio Lugo <jmanuellugo96@gmail.com>
2023-03-01 23:44:46 -0400

    Continue birthday party problem

--- codeforces/strange_birthday_party.cpp
+++ codeforces/strange_birthday_party.cpp
@@ -22,9 +22,18 @@ int main()
       cin >> k[i];
 
     for (int i{0}; i < m; i++)
+    {
       cin >> c[i];
+    }
 
-  
+    ull min_cost{0};
+
+    for (int i{0}; i < n; i++)
+    {
+      min_cost += c[k[i]];
+    }
+
+    cout << min_cost << "\n";
   }
 
   return 0;
