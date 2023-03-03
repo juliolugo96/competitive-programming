a1006c88b0897ad8d4f2cca82f2c6bac0e00c890
Julio Lugo <jmanuellugo96@gmail.com>
2023-03-03 11:33:08 -0400

    Solve strange birthday party

--- codeforces/strange_birthday_party.cpp
+++ codeforces/strange_birthday_party.cpp
@@ -9,6 +9,10 @@ ull c[300001]{0};
 
 int main()
 {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
   short t;
   cin >> t;
   while (t--)
@@ -22,16 +26,15 @@ int main()
       cin >> k[i];
 
     for (int i{0}; i < m; i++)
-    {
       cin >> c[i];
-    }
+
+    sort(begin(k), begin(k) + n);
+    reverse(begin(k), begin(k) + n);
 
     ull min_cost{0};
 
-    for (int i{0}; i < n; i++)
-    {
-      min_cost += c[k[i]];
-    }
+    for (int i{1}; i <= n; i++)
+      min_cost += i > k[i - 1] ? c[k[i - 1] - 1] : c[i - 1];
 
     cout << min_cost << "\n";
   }
