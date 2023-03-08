633b14117581a2da995675e9ab3df54a92011bfa
Julio Lugo <jmanuellugo96@gmail.com>
2023-03-08 00:00:15 -0400

    Start likes

---
+++ likes.cpp
@@ -0,0 +1,27 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int t;
+
+  cin >> t;
+
+  while(t--)
+  {
+    short n;
+
+    cin >> n;
+
+    int b[n]{0}, likes{0};
+
+    for(int i{0}; i < n; i++)
+      cin >> b[i];
+
+      
+  }
+
+
+  return 0;
+}
