d3e0a4bd64ab3193d59744b8fde05e8576327fa3
Julio Lugo <jmanuellugo96@gmail.com>
2023-03-09 23:15:04 -0400

    Solve likes

--- likes.cpp
+++ likes.cpp
@@ -1,4 +1,4 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -8,20 +8,48 @@ int main()
 
   cin >> t;
 
-  while(t--)
+  while (t--)
   {
     short n;
+    int dislikes{0};
 
     cin >> n;
 
-    int b[n]{0}, likes{0};
+    int b[n]{0};
 
-    for(int i{0}; i < n; i++)
+    for (int i{0}; i < n; i++)
+    {
       cin >> b[i];
 
-      
-  }
+      if (b[i] < 0)
+        dislikes++;
+    }
 
+    bool max = true;
+    for (int i{0}, k{0}; i < n; i++)
+    {
+      if (i >= n - dislikes)
+        cout << --k << " ";
+      else
+      {
+        cout << ++k << " ";
+      }
+    }
+    cout << "\n";
+    for (int i{0}, k{0}; i < n; i++)
+      if (i & 1 and dislikes)
+      {
+
+        dislikes--;
+        cout << --k << " ";
+      }
+      else
+      {
+        cout << ++k << " ";
+      }
+
+    cout << "\n";
+  }
 
   return 0;
 }
