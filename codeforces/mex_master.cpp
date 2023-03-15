0cce578b4de698ef374502a9b08fa6aff63075e0
Julio Lugo <jmanuellugo96@gmail.com>
2023-03-14 23:59:52 -0400

    Continue mex master

--- codeforces/mex_master.cpp
+++ codeforces/mex_master.cpp
@@ -18,17 +18,22 @@ int main()
     for (int i{0}; i < n; ++i)
       cin >> a[i];
 
-    for (int i{n - 1}; i > 0; --i)
-      a[i] = a[i] + a[i - 1];
-
     sort(begin(a), begin(a) + n);
 
     int mex{0};
 
     for (int i{1}; i < n; ++i)
+      cout << mex << " ";
+
+    cout << "\n";
+
+    for (int i{1}; i < n; ++i)
     {
       if (a[i] == mex)
+      {
         mex++;
+        continue;
+      }
 
       if (a[i] > mex)
         break;
