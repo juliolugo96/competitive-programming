0b75214844f940f6bd1968bdebaf0f723a1d9bc8
Julio Lugo <jmanuellugo96@gmail.com>
2023-03-17 11:25:28 -0400

    Solve sum of round numbers

--- codeforces/sum_of_round_numbers.cpp
+++ codeforces/sum_of_round_numbers.cpp
@@ -8,7 +8,31 @@ int main()
   cin >> t;
   while (t--)
   {
-    int n;
-    cin >> n;
+    string s;
+    cin >> s;
+
+    size_t n = s.size();
+    int count{0};
+    for (int i{0}; i < n; i++)
+    {
+      int c = s[i] - '0';
+
+      //      cout << "C: " << c << " ";
+
+      if (c != 0)
+        count++;
+    }
+    //   cout << "\n";
+    cout << count << "\n";
+
+    for (int i{0}; i < n; i++)
+    {
+
+      int c = s[i] - '0';
+
+      if (c != 0)
+        cout << c * (pow(10, n - i - 1)) << " ";
+    }
+    cout << "\n";
   }
 }
