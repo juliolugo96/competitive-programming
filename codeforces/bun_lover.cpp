f5a23063af4408c4995022edfe000a53818e89ea
Julio Lugo <jmanuellugo96@gmail.com>
2023-04-24 23:59:51 -0400

    Start bun lover

---
+++ codeforces/bun_lover.cpp
@@ -0,0 +1,22 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int t;
+
+  cin >> t;
+
+  while(t--)
+  {
+    int n;
+    cin >> n;
+    int res {26};
+    for (int i{0}; i < n; i++)
+      res += 26+i;
+  }
+
+
+  return 0;
+}
