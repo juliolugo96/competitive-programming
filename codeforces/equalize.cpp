b84e5f65c42b0ddbc0ecdcc84b5d346f61c8878b
Julio Lugo <jmanuellugo96@gmail.com>
2023-05-13 23:59:32 -0400

    Start equalize

---
+++ codeforces/equalize.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int n;
+
+  string s1, s2;
+
+  cin >> s1 >> s2;
+
+  auto s2_0 = count(s2.begin(), s2.end(), '0'), s2_1 = count(s2.begin(), s2.end(), '1');
+
+  cout << 0 << "\n";
+
+  return 0;
+}
